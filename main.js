ecb41dfa18d22ac0ed6337d900890e3bfdb929e5
NaiKit <kitkatkung2545@gmail.com>
2024-06-26 14:00:48 +0700

    first_commit

---
+++ main.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { WebcastPushConnection } = require("tiktok-live-connector");
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors({ origin: "*" }));
+
+app.get("/tiktok/:id", (req, res) => {
+  // tiktok connect
+  const tiktokUsername = req.params.id;
+  const tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
+
+  tiktokLiveConnection
+    .connect()
+    .then((state) => {
+      console.info(`Connected to roomId ${state.roomId}`);
+    })
+    .catch((err) => {
+      console.error("Failed to connect", err);
+    });
+
+  try {
+    // เชื่อมต่อแบบ SSE
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+      Connection: "keep-alive",
+    });
+    // Event comments
+    tiktokLiveConnection.on("chat", (data) => {
+      const message = `${data.uniqueId} (userId:${data.userId}) writes: ${data.comment}`;
+      // Respond with the message
+      res.write(`data: ${JSON.stringify(message)}\n\n`);
+    });
+
+    // ตัดการเชื่อมต่อแบบ SSE
+    req.on("close", () => {
+      tiktokLiveConnection.disconnect();
+      console.info(`Disconnected`);
+    });
+  } catch (error) {
+    console.error("Error handling request:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
